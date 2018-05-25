6aea3890e0aabee7591a76180f999df0f9906fd8
Zack Link <zlink@cylance.com>
2018-05-25 13:17:25 -0700

    replaced cylance with cy

--- CyPy.py
+++ CyPy.py
@@ -9,7 +9,7 @@ __license__ = "GPL"
 
 from cmd import Cmd
 import json
-import cylance
+import cylance as cy
 import subprocess
 
 class MyPrompt(Cmd):
@@ -25,19 +25,19 @@ class MyPrompt(Cmd):
         #print "len of args is " + str(len(args))
         # for x in args:
         #    print x
-        cylance.set_creds(*args)
+        cy.set_creds(*args)
 
     def do_saveConfig(self, arg):
         """Save authentication credentials to file.\nExample:\n saveConfig file=config.json"""
         args = arg.split("=")
         # TODO: Do some error checking here to see if this is a legit file path
-        cylance.save_creds(args[1])
+        cy.save_creds(args[1])
 
     def do_loadConfig(self, arg):
         """Load authentication credentials from file.\nExample:\n loadConfig file=config.json"""
         args = arg.split("=")
         # TODO: Do some error checking here to see if this is a legit file 
-        cylance.load_creds(args[1])
+        cy.load_creds(args[1])
 
     def do_shell(self, args):
         """shell <command> <args>\n run a shell command, e.g. cat users.csv | grep <email>"""
@@ -48,12 +48,12 @@ class MyPrompt(Cmd):
     
     def do_getUsers(self, args):
         """Get all Users.\nOptions:\n  email=<string> (contains <string>)\n  first_name=<string>\n  last_name=<string>\n  has_logged_in=<True|False>\n  user_role=<role> (e.g. Administrator is 00000000-0000-0000-0000-000000000002)\n  date_last_login=<date> (e.g.  2018-01- for Jan 2018)\n  date_email_confirmed=<date> (e.g.  2018-01- for Jan 2018)\n  date_created=<date> (e.g.  2018-01- for Jan 2018)\n  date_modified=<date> (e.g.  2018-01- for Jan 2018)\n  out=<filename> (suports .json and .csv)\nOptions should be in the format: <field1>=<value1>,<field2>=<value2>,etc"""
-        print cylance.get_data("USERS", args)
+        print cy.get_data("USERS", args)
 
     def do_getUser(self, args):
         """Get User by ID.\n getUser id=<id> or getUser email=<email address>"""
         fields = args.split("=")
-        print cylance.get_data_by_id("USERS", fields[1])
+        print cy.get_data_by_id("USERS", fields[1])
 
     def do_deleteUser(self, args):
         """Delete User by ID.\n deleteUser id=XXX\n deleteUser in=users.csv"""
@@ -64,25 +64,25 @@ class MyPrompt(Cmd):
             with open(fields[1]) as f:
                 line = f.readlines().strip()
                 if len(lines) == 36:
-                    print cylance.delete_data("USERS", line)
+                    print cy.delete_data("USERS", line)
         elif fields[0] == 'id':
-            print cylance.delete_data("USERS", fields[1])
+            print cy.delete_data("USERS", fields[1])
 
     def do_updateUser(self, args):
         """Update User by ID.\n updateUser id=XXX in=user.json"""
         all_args = args.split()
         id_k,id_v = all_args[0].split("=")
         # print "updating user id = " + id_v
-        print cylance.update_data("USERS", all_args[0], all_args[1])
+        print cy.update_data("USERS", all_args[0], all_args[1])
 
     def do_getDevices(self, args):
         """Get all Devices.\nOptions:\n  name=<string> (i.e. name contains <string>)\n  date_first_registered=<date> (e.g.  2018-01- for Jan 2018)\n  id=<devicce id>\n  version=<version>\n  state=<Online|Offline>\n  out=<filename> (suports .json and .csv)"""
-        print cylance.get_data("DEVICES", args)
+        print cy.get_data("DEVICES", args)
 
     def do_getDevice(self, args):
         """Get Device by ID.\n getDevice id=<id>\n"""
         fields = args.split("=")
-        print cylance.get_data_by_id("DEVICES", fields[1])
+        print cy.get_data_by_id("DEVICES", fields[1])
 
     def do_deleteDevice(self, args):
         """Delete Device by ID.\n deleteDevice id=XXX\n deleteDevice in=devices.csv"""
@@ -93,42 +93,42 @@ class MyPrompt(Cmd):
             with open(fields[1]) as f:
                 line = f.readlines().strip()
                 if len(lines) == 36:
-                    print cylance.delete_data("DEVICES", line)
+                    print cy.delete_data("DEVICES", line)
         elif fields[0] == 'id':
-            print cylance.delete_data("DEVICES", fields[1])
+            print cy.delete_data("DEVICES", fields[1])
 
     def do_getPolicies(self, args):
         """Get all Policies.\nOptions:\n  name=<string> (i.e. name contains <string>)\n  <field2=XXX>\n  <field3=YYY>\n  <field4=ZZZ>\n  out=<filename> (suports .json and .csv)"""
         # TODO fix comments above for help
-        print cylance.get_data("POLICIES", args)
+        print cy.get_data("POLICIES", args)
 
     def do_getPolicy(self, args):
         """Get Policy by ID.\nExample:\n getPolicy id=XYZ out=policy.json"""
         fields = args.split("=")
-        print cylance.get_data_by_id("POLICIES", fields[1])
+        print cy.get_data_by_id("POLICIES", fields[1])
 
     def do_getZones(self, args):
         """Get all Zones.\nOptions:\n  name=<string> (i.e. name contains <string>)\n  <field2=XXX>\n  <field3=YYY>\n  <field4=ZZZ>\n  out=<filename> (suports .json and .csv)"""
         # TODO fix comments above for help
-        print cylance.get_data("ZONES", args)
+        print cy.get_data("ZONES", args)
 
     def do_getThreats(self, args):
         """Get all Threats.\nOptions:\n  name=<string> (i.e. name contains <string>)\n  <field2=XXX>\n  <field3=YYY>\n  <field4=ZZZ>\n  out=<filename> (suports .json and .csv)"""
         # TODO fix comments above for help
-        print cylance.get_data("THREATS", args)
+        print cy.get_data("THREATS", args)
 
     def do_getRegions(self, args):
         """Get available geographical regions"""
-        for x in cylance.get_regions():
+        for x in cy.get_regions():
             print x
 
     def do_getCurrentRegion(self, args):
         """Get current setting for selected region"""
-        print cylance.get_region()
+        print cy.get_region()
 
     def do_setRegion(self, args):
         """"Set region.  For a list of available regions use 'getRegions'"""
-        cylance.set_region(args)
+        cy.set_region(args)
 
     def do_filtering(self, args):
         """All top level attributes can be filtered on.  Multiple filters can be used by putting a comma (no spaces) in between the filters.  Search string uses contains, not exact match.  Searches are case-insensitive\nExample: getDevices state=Online,name=US\n"""
